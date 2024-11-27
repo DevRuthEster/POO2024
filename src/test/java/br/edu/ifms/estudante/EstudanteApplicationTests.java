9fbf2e7ec032c6f6ae7a4e6c1d2dd31ba24cb08c
DevRuthEster <125623470+DevRuthEster@users.noreply.github.com>
2024-11-26 21:49:14 -0400

    Add files via upload

---
+++ src/test/java/br/edu/ifms/estudante/EstudanteApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifms.estudante;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstudanteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
